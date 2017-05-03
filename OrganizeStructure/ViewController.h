d5e4f06ce0b7c20488e3e9d5f53c71432d05b491
666gps <yang@yang.local>
2017-05-03 17:36:45 +0800

    Initial Commit

---
+++ OrganizeStructure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OrganizeStructure
+//
+//  Created by 666gps on 2017/5/3.
+//  Copyright © 2017年 666gps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
